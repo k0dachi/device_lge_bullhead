10473784a2fc03c1c14ba5a5b4747529d032eacd
Daniel Micay <danielmicay@gmail.com>
2016-01-17 01:00:28 -0500

    switch to in-tree kernel builds

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -16,5 +16,18 @@
 
 LOCAL_PATH := $(call my-dir)
 
+KERNEL_DEFCONFIG := bullhead_defconfig
+KERNEL_DIR := kernel/lge/bullhead
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+
 $(call add-radio-file,radio/bootloader.img)
 $(call add-radio-file,radio/radio.img)
